3550cb691a622d6557934a8c417b4c16696e5a5d
shashank-zs <zs@92146>
2023-03-15 01:49:13 +0530

    cvd

---
+++ src/test/java/testing/testing.java
@@ -0,0 +1,13 @@
+package testing;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testing {
+    @Test
+    public static void main(){
+        System.out.println("shashank");
+        String shan ="shan1";
+        Assert.assertEquals(shan,"shan1");
+    }
+}
